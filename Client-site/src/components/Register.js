0accd68bdd9e4b398e98585ea95503efa10c9596
nitindpatil15 <nitindpatil2002@gmail.com>
2024-04-29 22:24:37 +0530

    production

--- Client-site/src/components/Register.js
+++ Client-site/src/components/Register.js
@@ -16,7 +16,7 @@ const Register = (props) => {
     const { name, email, password } = createuser;
     try {
       const response = await fetch(
-        `http://localhost:5000/api/auth/createuser/`,
+        `https://notebook-k05h.onrender.com/api/auth/createuser/`,
         {
           method: "POST",
           headers: {
--- Client-site/src/context/NoteState.js
+++ Client-site/src/context/NoteState.js
@@ -2,8 +2,7 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000";
-  // const host = "https://inotebook-backend-jade.vercel.app/";
+  const host = "https://notebook-k05h.onrender.com/";
   const notesinitial = [];
   const [notes, setNotes] = useState(notesinitial);
 
